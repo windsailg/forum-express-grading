27948f1d00df13fd5e78fa1317ae15ed6933ab5b
REN <windsailg@gmail.com>
2020-12-03 22:27:19 +0800

    fix seeder problem

--- seeders/20201203113221-comment-seed-file.js
+++ seeders/20201203113221-comment-seed-file.js
@@ -2,33 +2,33 @@
 const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Comment', [{
+    await queryInterface.bulkInsert('Comments', [{
       text: faker.lorem.text(),
       UserId: 1,
       RestaurantId: 61,
       createdAt: new Date(),
-      upDateAt: new Date()
+      updatedAt: new Date()
     }, {
       text: faker.lorem.text(),
       UserId: 2,
       RestaurantId: 11,
       createdAt: new Date(),
-      upDateAt: new Date()
+      updatedAt: new Date()
     }, {
       text: faker.lorem.text(),
       UserId: 2,
       RestaurantId: 121,
       createdAt: new Date(),
-      upDateAt: new Date()
+      updatedAt: new Date()
     }, {
       text: faker.lorem.text(),
       UserId: 3,
       RestaurantId: 41,
       createdAt: new Date(),
-      upDateAt: new Date()
+      updatedAt: new Date()
     }], {})
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Comment', null, {})
+    await queryInterface.bulkDelete('Comments', null, {})
   }
 }
