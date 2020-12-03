968ec40842228dbbf4ea27979eb09cffda506847
REN <windsailg@gmail.com>
2020-12-03 23:06:34 +0800

    try fix seeder

--- seeders/20201203113221-comment-seed-file.js
+++ seeders/20201203113221-comment-seed-file.js
@@ -1,31 +1,27 @@
 'use strict'
-const faker = require('faker')
+// const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Comments', [{
-      id: 1,
-      text: faker.lorem.text(),
+      text: 'lorem',
       UserId: 1,
       RestaurantId: 61,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      id: 2,
-      text: faker.lorem.text(),
+      text: 'lorem',
       UserId: 2,
       RestaurantId: 11,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      id: 3,
-      text: faker.lorem.text(),
+      text: 'lorem',
       UserId: 2,
       RestaurantId: 121,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      id: 4,
-      text: faker.lorem.text(),
+      text: 'lorem',
       UserId: 3,
       RestaurantId: 41,
       createdAt: new Date(),
