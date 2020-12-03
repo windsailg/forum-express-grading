385457ab992cd7cd66d67f7d333ff2bf0b93b551
REN <windsailg@gmail.com>
2020-12-04 00:24:15 +0800

    try fix seeder

--- seeders/20201203113221-comment-seed-file.js
+++ seeders/20201203113221-comment-seed-file.js
@@ -4,25 +4,21 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Comments', [{
       text: 'lorem',
-      UserId: 1,
       RestaurantId: 61,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       text: 'lorem',
-      UserId: 2,
       RestaurantId: 11,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       text: 'lorem',
-      UserId: 2,
       RestaurantId: 121,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       text: 'lorem',
-      UserId: 3,
       RestaurantId: 41,
       createdAt: new Date(),
       updatedAt: new Date()
