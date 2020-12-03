267f144019ddb563fd1027826ccbd8ed0e0711d6
REN <windsailg@gmail.com>
2020-12-03 20:24:22 +0800

    fix user img column problem

--- seeders/20201203121144-users-seed-file.js
+++ seeders/20201203121144-users-seed-file.js
@@ -7,6 +7,7 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: true,
       name: 'root',
+      image: '',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
@@ -14,6 +15,7 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user1',
+      image: '',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
@@ -21,6 +23,7 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user2',
+      image: '',
       createdAt: new Date(),
       updatedAt: new Date()
     }], {})
