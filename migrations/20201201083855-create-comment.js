4019515de56b77315f12fbca0bad9b50c1ce7876
REN <windsailg@gmail.com>
2020-12-03 20:29:10 +0800

    edit procfile

--- migrations/20201201083855-create-comment.js
+++ migrations/20201201083855-create-comment.js
@@ -1,4 +1,5 @@
-'use strict';
+'use strict'
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Comments', {
@@ -30,4 +31,4 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Comments')
   }
-};
+}
