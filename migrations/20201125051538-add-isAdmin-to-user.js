3999a69af92411778c8c50afb21e78d50cfc0608
REN <windsailg@gmail.com>
2020-12-04 00:05:04 +0800

    try fix seeder

--- migrations/20201125051538-add-isAdmin-to-user.js
+++ migrations/20201125051538-add-isAdmin-to-user.js
@@ -1,10 +1,10 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Users', 'isAdmin', {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false
-    })
+    // await queryInterface.addColumn('Users', 'isAdmin', {
+    //   type: Sequelize.BOOLEAN,
+    //   defaultValue: false
+    // })
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn('Users', 'isAdmin')
