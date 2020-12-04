349cbf6bdf5c8f8faa4f75590f8ecb70b60e1883
REN <windsailg@gmail.com>
2020-12-04 19:54:57 +0800

    edit migrate file

--- migrations/20201201083855-create-comment.js
+++ migrations/20201201083855-create-comment.js
@@ -2,31 +2,31 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Comments',{
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      text: {
-        type: Sequelize.STRING
-      },
-      UserId: {
-        type: Sequelize.INTEGER
-      },
-      RestaurantId: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    })
+    // await queryInterface.createTable('Comments',{
+    //   id: {
+    //     allowNull: false,
+    //     autoIncrement: true,
+    //     primaryKey: true,
+    //     type: Sequelize.INTEGER
+    //   },
+    //   text: {
+    //     type: Sequelize.STRING
+    //   },
+    //   UserId: {
+    //     type: Sequelize.INTEGER
+    //   },
+    //   RestaurantId: {
+    //     type: Sequelize.INTEGER
+    //   },
+    //   createdAt: {
+    //     allowNull: false,
+    //     type: Sequelize.DATE
+    //   },
+    //   updatedAt: {
+    //     allowNull: false,
+    //     type: Sequelize.DATE
+    //   }
+    // })
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Comments')
--- migrations/20201201184705-add-image-to-users.js
+++ migrations/20201201184705-add-image-to-users.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Users','image', {
-      type: Sequelize.STRING
-    })
+    // await queryInterface.addColumn('Users','image', {
+    //   type: Sequelize.STRING
+    // })
   },
 
   down: async (queryInterface, Sequelize) => {
