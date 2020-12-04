a7828d49df57fe044c6db629636727462d9af6a3
REN <windsailg@gmail.com>
2020-12-04 19:56:54 +0800

    edit migrate file edit viewCount

--- migrations/20201204101328-add-viewcounts-to-restaurant.js
+++ migrations/20201204101328-add-viewcounts-to-restaurant.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // await queryInterface.addColumn('Restaurants','viewCount', {
-    //   type: Sequelize.INTEGER
-    // })
+    await queryInterface.addColumn('Restaurants','viewCount', {
+      type: Sequelize.INTEGER
+    })
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn('Restaurants','viewCount')
