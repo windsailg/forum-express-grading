4b5bb7ed41eaea567c7a62da57136894a4fe0722
REN <windsailg@gmail.com>
2020-12-04 19:43:20 +0800

    edit migrate file

--- migrations/20201130054441-create-category.js
+++ migrations/20201130054441-create-category.js
@@ -1,25 +1,25 @@
 'use strict'
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Categories', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    })
+    // await queryInterface.createTable('Categories', {
+    //   id: {
+    //     allowNull: false,
+    //     autoIncrement: true,
+    //     primaryKey: true,
+    //     type: Sequelize.INTEGER
+    //   },
+    //   name: {
+    //     type: Sequelize.STRING
+    //   },
+    //   createdAt: {
+    //     allowNull: false,
+    //     type: Sequelize.DATE
+    //   },
+    //   updatedAt: {
+    //     allowNull: false,
+    //     type: Sequelize.DATE
+    //   }
+    // })
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Categories')
